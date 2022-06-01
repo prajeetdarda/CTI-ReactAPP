35daff06d82e06f17e998b935ff436611d73acfd
prajeetdarda <55790241+prajeetdarda@users.noreply.github.com>
2022-06-01 15:57:32 +0530

    Updates

---
+++ precache-manifest.c452bdf2289bf6a08211c1e5b969b201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fba6102aaf4ae5c3b197",
+    "url": "/CTI-ReactAPP/static/css/main.28c9626a.chunk.css"
+  },
+  {
+    "revision": "fba6102aaf4ae5c3b197",
+    "url": "/CTI-ReactAPP/static/js/main.88067f6c.chunk.js"
+  },
+  {
+    "revision": "4ee22fcda578556b86b8",
+    "url": "/CTI-ReactAPP/static/js/runtime~main.ab4d1548.js"
+  },
+  {
+    "revision": "104baa6c17a7c828d319",
+    "url": "/CTI-ReactAPP/static/js/2.244a6dc2.chunk.js"
+  },
+  {
+    "revision": "1019f9b1845f1f2b9306",
+    "url": "/CTI-ReactAPP/static/js/3.44bbfbbc.chunk.js"
+  },
+  {
+    "revision": "0e87e66fbe49ac95baad32e9f80a5f32",
+    "url": "/CTI-ReactAPP/index.html"
+  }
+];
