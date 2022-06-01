5e920bc73372932c77b5d7753c3594b505fd5cca
prajeetdarda <55790241+prajeetdarda@users.noreply.github.com>
2022-06-01 16:02:15 +0530

    Updates

---
+++ precache-manifest.acb6df1653163fdec3f468e2c1ae6db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fba6102aaf4ae5c3b197",
+    "url": "/CTI-ReactAPP/tlapp/static/css/main.28c9626a.chunk.css"
+  },
+  {
+    "revision": "fba6102aaf4ae5c3b197",
+    "url": "/CTI-ReactAPP/tlapp/static/js/main.88067f6c.chunk.js"
+  },
+  {
+    "revision": "2cd71c39047e118e6d83",
+    "url": "/CTI-ReactAPP/tlapp/static/js/runtime~main.0d0dd8f0.js"
+  },
+  {
+    "revision": "104baa6c17a7c828d319",
+    "url": "/CTI-ReactAPP/tlapp/static/js/2.244a6dc2.chunk.js"
+  },
+  {
+    "revision": "1019f9b1845f1f2b9306",
+    "url": "/CTI-ReactAPP/tlapp/static/js/3.44bbfbbc.chunk.js"
+  },
+  {
+    "revision": "7df828878be75c17bcd1e1b0a9b71c37",
+    "url": "/CTI-ReactAPP/tlapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CTI-ReactAPP/precache-manifest.c452bdf2289bf6a08211c1e5b969b201.js"
+  "/CTI-ReactAPP/tlapp/precache-manifest.acb6df1653163fdec3f468e2c1ae6db8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/CTI-ReactAPP/index.html", {
+workbox.routing.registerNavigationRoute("/CTI-ReactAPP/tlapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
